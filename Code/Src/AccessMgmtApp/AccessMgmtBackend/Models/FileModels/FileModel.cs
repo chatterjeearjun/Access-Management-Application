e78b03c9d0f29399563f3ee1332b0bf276097ef8
Arjun Chatterjee <chatterjeearjun.cse@gmail.com>
2022-03-22 17:05:28 +0530

    Added file upload endpoint

---
+++ Code/Src/AccessMgmtApp/AccessMgmtBackend/Models/FileModels/FileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AccessMgmtBackend.Models
+{
+    public class FileModel
+    {
+        public IFormFile File { get; set; }
+        public string company_identifier { get; set; }
+        public string user_identifier { get; set; }
+    }
+}
