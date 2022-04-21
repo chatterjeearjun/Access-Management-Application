2b1a6232c39810df9c713f9123cc5a7c604e8196
Arjun Chatterjee <32008283+chatterjeearjun@users.noreply.github.com>
2022-04-21 20:14:14 +0530

    Bug fixes

--- Code/Src/AccessMgmtApp/AccessMgmtBackend/Controllers/DataImportController.cs
+++ Code/Src/AccessMgmtApp/AccessMgmtBackend/Controllers/DataImportController.cs
@@ -61,82 +61,88 @@ namespace AccessMgmtBackend.Controllers
                             dtEmployeeRecords.Rows.Remove(rowToromove);
                             foreach (DataRow row in dtEmployeeRecords.Rows)
                             {
-                                Employee employee = new Employee();
-                                employee.company_identifier = row[0].ToString();
-                                employee.emp_role = row[1].ToString();
-                                employee.emp_group = row[2].ToString();
-                                employee.emp_designation = row[3].ToString();
-                                employee.emp_first_name = row[4].ToString();
-                                employee.emp_last_name = row[5].ToString();
-                                employee.emp_email = row[6].ToString();
-                                employee.emp_office_phone = row[7].ToString();
-                                employee.emp_mobile_number = row[8].ToString();
-                                employee.emp_dob = row[9] != null ? Convert.ToDateTime(row[9]) : DateTime.MinValue;
-                                employee.emp_joining_date = row[9] != null ? Convert.ToDateTime(row[9]) : DateTime.MinValue;
-                                employee.emp_relieving_date = row[9] != null ? Convert.ToDateTime(row[9]) : DateTime.MaxValue;
-                                employee.associated_assets = row[12].ToString();
-                                employee.emp_approval_overdue = row[9] != null ? Convert.ToDateTime(row[9]) : DateTime.MaxValue;
-                                employee.created_date = DateTime.UtcNow;
-                                employee.created_by = "Application";
-                                employee.is_active = true;
-                                _companyContext.Employees.Add(employee);
-
-                                if (!string.IsNullOrEmpty(employee.emp_role))
+                                bool isExistingEmployee = false;
+                                isExistingEmployee = _companyContext.Employees.Where(x=>x.company_identifier == row[0].ToString() && 
+                                x.emp_email.ToLower() == row[6].ToString().ToLower()).Count() > 0;
+                                if (!isExistingEmployee)
                                 {
-                                    string[] roles = employee.emp_role.Split(',');
-                                    foreach (var role in roles)
+                                    Employee employee = new Employee();
+                                    employee.company_identifier = row[0].ToString();
+                                    employee.emp_role = row[1].ToString();
+                                    employee.emp_group = row[2].ToString();
+                                    employee.emp_designation = row[3].ToString();
+                                    employee.emp_first_name = row[4].ToString();
+                                    employee.emp_last_name = row[5].ToString();
+                                    employee.emp_email = row[6].ToString();
+                                    employee.emp_office_phone = row[7].ToString();
+                                    employee.emp_mobile_number = row[8].ToString();
+                                    employee.emp_dob = row[9] != null ? Convert.ToDateTime(row[9]) : DateTime.MinValue;
+                                    employee.emp_joining_date = row[9] != null ? Convert.ToDateTime(row[9]) : DateTime.MinValue;
+                                    employee.emp_relieving_date = row[9] != null ? Convert.ToDateTime(row[9]) : DateTime.MaxValue;
+                                    employee.associated_assets = row[12].ToString();
+                                    employee.emp_approval_overdue = row[9] != null ? Convert.ToDateTime(row[9]) : DateTime.MaxValue;
+                                    employee.created_date = DateTime.UtcNow;
+                                    employee.created_by = "Application";
+                                    employee.is_active = true;
+                                    _companyContext.Employees.Add(employee);
+
+                                    if (!string.IsNullOrEmpty(employee.emp_role))
                                     {
-                                        _companyContext.EmployeeToRoles.Add(new EmployeeToRole
+                                        string[] roles = employee.emp_role.Split(',');
+                                        foreach (var role in roles)
                                         {
-                                            id = 0,
-                                            company_identifier = employee.company_identifier,
-                                            employee_identifier = employee.employee_identifier.ToString(),
-                                            role_identifier = role.ToString(),
-                                            is_active = true,
-                                            created_date = DateTime.UtcNow,
-                                            created_by = "Application"
-                                        });
-                                        var associatedRolesAssets = _companyContext.AssetToRoles.Where(s => s.role_identifier == role && s.company_identifier == employee.company_identifier && s.is_active == true).ToList()
-                                        .Select(x => x.asset_identifier);
-                                        employee.associated_assets = (!string.IsNullOrEmpty(employee.associated_assets)) ?
-                                            employee.associated_assets + "," + String.Join(",", associatedRolesAssets) : String.Join(",", associatedRolesAssets);
+                                            _companyContext.EmployeeToRoles.Add(new EmployeeToRole
+                                            {
+                                                id = 0,
+                                                company_identifier = employee.company_identifier,
+                                                employee_identifier = employee.employee_identifier.ToString(),
+                                                role_identifier = role.ToString(),
+                                                is_active = true,
+                                                created_date = DateTime.UtcNow,
+                                                created_by = "Application"
+                                            });
+                                            var associatedRolesAssets = _companyContext.AssetToRoles.Where(s => s.role_identifier == role && s.company_identifier == employee.company_identifier && s.is_active == true).ToList()
+                                            .Select(x => x.asset_identifier);
+                                            employee.associated_assets = (!string.IsNullOrEmpty(employee.associated_assets)) ?
+                                                employee.associated_assets + "," + String.Join(",", associatedRolesAssets) : String.Join(",", associatedRolesAssets);
+                                        }
+                                    }
+                                    if (!string.IsNullOrEmpty(employee.emp_group))
+                                    {
+                                        string[] groups = employee.emp_group.Split(',');
+                                        foreach (var group in groups)
+                                        {
+                                            _companyContext.EmployeeToGroups.Add(new EmployeeToGroup
+                                            {
+                                                id = 0,
+                                                company_identifier = employee.company_identifier,
+                                                employee_identifier = employee.employee_identifier.ToString(),
+                                                group_identifier = group.ToString(),
+                                                is_active = true,
+                                                created_date = DateTime.UtcNow,
+                                                created_by = "Application"
+                                            });
+                                        }
+                                    }
+
+                                    if (!string.IsNullOrEmpty(employee.associated_assets))
+                                    {
+                                        string[] assets = employee.associated_assets.Split(',');
+                                        foreach (var asset in assets.Distinct())
+                                        {
+                                            _companyContext.AssetToEmployees.Add(new AssetToEmployee
+                                            {
+                                                id = 0,
+                                                company_identifier = employee.company_identifier,
+                                                asset_identifier = asset.ToString(),
+                                                employee_identifier = employee.employee_identifier.ToString(),
+                                                is_active = true,
+                                                created_date = DateTime.UtcNow,
+                                                created_by = "Application"
+                                            });
+                                        }
                                     }
                                 }
-                                if (!string.IsNullOrEmpty(employee.emp_group))
-                                {
-                                    string[] groups = employee.emp_group.Split(',');
-                                    foreach (var group in groups)
-                                    {
-                                        _companyContext.EmployeeToGroups.Add(new EmployeeToGroup
-                                        {
-                                            id = 0,
-                                            company_identifier = employee.company_identifier,
-                                            employee_identifier = employee.employee_identifier.ToString(),
-                                            group_identifier = group.ToString(),
-                                            is_active = true,
-                                            created_date = DateTime.UtcNow,
-                                            created_by = "Application"
-                                        });
-                                    }
-                                }
-
-                                if (!string.IsNullOrEmpty(employee.associated_assets))
-                                {
-                                    string[] assets = employee.associated_assets.Split(',');
-                                    foreach (var asset in assets.Distinct())
-                                    {
-                                        _companyContext.AssetToEmployees.Add(new AssetToEmployee
-                                        {
-                                            id = 0,
-                                            company_identifier = employee.company_identifier,
-                                            asset_identifier = asset.ToString(),
-                                            employee_identifier = employee.employee_identifier.ToString(),
-                                            is_active = true,
-                                            created_date = DateTime.UtcNow,
-                                            created_by = "Application"
-                                        });
-                                    }
-                                }                            
                             }
                             //_companyContext.BulkInsert(employees, options =>
                             //{
@@ -147,7 +153,7 @@ namespace AccessMgmtBackend.Controllers
                             if (output > 0)
                                 message = "The Excel file has been successfully uploaded.";
                             else
-                                message = "Something Went Wrong!, The Excel file uploaded has fiald.";
+                                message = "Something Went Wrong!, The Excel file uploaded has failed.";
                         }
                         else
                             message = "Selected file is empty.";
